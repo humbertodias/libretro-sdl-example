fa9fd1e998807a309955df23787932f9a6bac8d8
Humberto Dias <hldtux@gmail.com>
2025-02-05 09:12:30 -0300

    typo

--- game.cpp
+++ game.cpp
@@ -4,29 +4,18 @@ Game::Game(unsigned int screen_width, unsigned int screen_height) {
     this->screen_width = screen_width;
     this->screen_height = screen_height;
 
-    if (SDL_Init(SDL_INIT_VIDEO)) {
+    if (SDL_Init(SDL_INIT_VIDEO) == 0) {
         throw "SDL Initialization failed";
     }
 
-    window = SDL_CreateWindow("Hello World", screen_width, screen_height, SDL_WINDOW_RESIZABLE);
-    if (!window) {
-        throw "Failed to create window";
-    }
+    surface = SDL_CreateSurface(screen_width, screen_height, SDL_PIXELFORMAT_RGBA32);
 
     if (USE_HARDWARE) {
+        window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOW_RESIZABLE);
         renderer = SDL_CreateRenderer(window, nullptr);
-        if (!renderer) {
-            throw "Failed to create renderer";
-        }
-
-        // Ativar VSync
         SDL_SetRenderVSync(renderer, 1);
     } else {
         renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
-    }
-
-    if (!renderer) {
-        throw "Failed to create renderer";
     }
 
     SDL_Surface* pipe = SDL_LoadBMP("image.bmp");
@@ -43,21 +32,13 @@ void Game::GameLoop(double delta_time) {
 }
 
 uint32_t* Game::GetFrameBuffer() {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
+    SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
+    SDL_RenderClear( renderer );
     SDL_RenderTexture(renderer, pipe, nullptr, nullptr);
-
     SDL_RenderPresent(renderer);
-
-    SDL_Surface* surface = SDL_CreateSurface(screen_width, screen_height, SDL_PIXELFORMAT_ARGB8888);
-    if (!surface) {
-        throw "Failed to create surface";
+    if (USE_HARDWARE) {
+        SDL_RenderReadPixels(renderer, nullptr);
     }
-
-    SDL_SaveBMP(surface, "screenshot.bmp");
-    uint32_t* pixels = static_cast<uint32_t*>(surface->pixels);
-    SDL_DestroySurface(surface);
-
-    return pixels;
+    SDL_SaveBMP(surface, "screenshot.bmp");  // For demo's sake
+    return (uint32_t*)surface->pixels;
 }
